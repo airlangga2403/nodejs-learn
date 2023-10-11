6113fb5b209122c6aae8afb4c25a8825fd0a064a
Zeeus <airlanggapermana96@gmail.com>
2023-10-11 19:23:47 +0700

    add

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdhandler, editNoteByIdHandler, deleteNoteHandlerById } = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+        options: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdhandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteHandlerById,
+    }
+
+];
+
+module.exports = routes;
